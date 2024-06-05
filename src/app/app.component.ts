6b1bbd4fd47185b26b08e0a8cc9c4d661e6e5370
johnnyRobot <beitschalbua@gmx.at>
2024-06-05 16:22:32 +0200

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wishlist';
+  items: WishItem[] = [
+    new WishItem("new phone"),
+    new WishItem("new laptop", true),
+    new WishItem("new car"),
+  ]
+  title = 'this is my wishlist';
 }
---
+++ src/shared/models/wishItem.ts
@@ -0,0 +1,6 @@
+export class WishItem {
+    constructor(public wishText: string, public isComplete: boolean = false) {
+        // parameter properties is a TypeScript feature that allows you to define properties and assign them in the constructor
+        // TypeScript shorthand for defining properties and assigning them in the constructor
+    }
+}
