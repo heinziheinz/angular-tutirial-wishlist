2dde2f44db7aa87bb0f455e0099cd04350729494
johnnyRobot <beitschalbua@gmx.at>
2024-06-06 12:59:41 +0200

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,4 +18,29 @@ export class AppComponent {
     new WishItem("new car"),
   ]
   title = 'this is my wishlist';
+  visibleItems = this.items;
+  listFilter = '0';
+
+  newWishText = '';
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+    console.log('add new wish');
+  }
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
+
+  filterChanged(value: string) {
+    console.log(value);
+    if (value === '0') {
+      this.visibleItems = this.items;
+    } else if (value === '1') {
+      this.visibleItems = this.items.filter(item => !item.isComplete);
+    } else {
+      this.visibleItems = this.items.filter(item => item.isComplete);
+    }
+  }
 }
