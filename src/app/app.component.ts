f79fd43b1292e04bffa1f28047cd4a254f9f60d8
johnnyRobot <beitschalbua@gmx.at>
2024-06-07 11:35:22 +0200

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { FormsModule } from '@angular/forms';
 
+
+const filters = [
+  (item: WishItem) => true,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete
+];
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -18,8 +24,21 @@ export class AppComponent {
     new WishItem("new car"),
   ]
   title = 'this is my wishlist';
-  visibleItems = this.items;
-  listFilter = '0';
+  // visibleItems = this.items;
+  get visibleItems(): WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+    // wehenever a new value is assigned to listFilter, this getter is called
+    // let value = this.listFilter;
+
+    // if (value === '0') {
+    //   return this.items;
+    // } else if (value === '1') {
+    //   return this.items.filter(item => !item.isComplete);
+    // } else {
+    //   return this.items.filter(item => item.isComplete);
+    // }
+  }
+  listFilter: any = '0';
 
   newWishText = '';
   addNewWish() {
@@ -32,16 +51,5 @@ export class AppComponent {
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
     console.log(item);
-  }
-
-  filterChanged(value: string) {
-    console.log(value);
-    if (value === '0') {
-      this.visibleItems = this.items;
-    } else if (value === '1') {
-      this.visibleItems = this.items.filter(item => !item.isComplete);
-    } else {
-      this.visibleItems = this.items.filter(item => item.isComplete);
-    }
   }
 }
